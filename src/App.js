53b93b7e421b47c860b21e9622250a1c9fa7ea3c
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-03-27 17:54:24 +0500

    Create about part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from "./componints/Header/header"
+import About from "./componints/About/about"
+
 
 function App() {
   return (
     <>
       <Header />
+      <About />
     </>
   )
 }
---
+++ src/componints/About/about.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom'
+
+import "./about.css"
+import MeImg from "../../images/meImg.jpg"
+
+
+function About () {
+
+  return (
+    <main className="about">
+      <div className="container about__continer">
+        <div className="about__lift">
+          <p className="about__left-text">My name is <span className="about__me-name">Sharofov Muhammadbobur</span>, I am 19 years old. I currently live in Tashkent and study at the Najot Ta'lim training center.</p>
+          <p className="about__left-text">During my studies, I created various websites. The main technologies I know are <span className="about__me-technologies">HTML, CSS, Bootstarap, Scss, JavaScript, NodeJS, Rest API Express, Postgres.</span></p>
+          <p className="about__left-text">If you want to know more, write to my telegram: <br/><Link to="#" className="about__me-telegram">t.me/MuhammadboburDeveloper</Link></p>
+          <p className="about__left-text">Subscribe to my social media blogs:</p>
+
+          <div className="about__social_networks">
+            <Link to="#" className="about__social_network">
+              <img className="about__social_networ-icon" src="https://img.icons8.com/nolan/64/telegram-app.png" alt="social_networ-icon" />
+            </Link>
+            <Link to="#" className="about__social_network">
+            <img className="about__social_networ-icon" src="https://img.icons8.com/nolan/64/twitter.png" alt="social_networ-icon" />
+            </Link>
+            <Link to="#" className="about__social_network">
+            <img className="about__social_networ-icon" src="https://img.icons8.com/nolan/64/github.png" alt="social_networ-icon" />
+            </Link>
+          </div>
+        </div>
+
+        <img className="about__me-img" src={MeImg} alt="Me img" width="400" />
+      </div>
+    </main>
+  )
+}
+
+export default About
